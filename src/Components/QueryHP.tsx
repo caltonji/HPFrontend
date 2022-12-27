06756f379289f282a2f0a1bf4cb58b022d32633a
caltonji <caltonji@gmail.com>
2022-12-26 18:37:14 -0600

    Setup for the new branch

--- src/Components/QueryHP.tsx
+++ src/Components/QueryHP.tsx
@@ -4,41 +4,33 @@ import {
     Grid,
     CircularProgress,
     Typography
-    }
+}
     from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import * as React from 'react';
 import axios from 'axios';
 import './QueryHP.css';
 
-interface APIResponse {
-    responses: IResponse[]
-}
-
-interface IResponse {
-    answer: string,
-    passage: string,
-    start: number,
-    end: number,
-    score: number
-}
-
 interface IQueryHPState {
     query: string,
-    lastResponse: IResponse | null,
-    loading: boolean
+    answer: string,
+    loading: boolean,
+    options: string[]
 }
-const title = "Ask a Trivia Question about Harry Potter and the Sorcerer's Stone.";
-const base_uri = "http://harrypotterb2.azurewebsites.net"
+const title = "Harry Potter Trivia Bot.";
+const base_uri = "https://harrypotterf1.azurewebsites.net"
 
 export default class QueryHP extends React.Component<any, IQueryHPState> {
+    private timeout: any;
 
     constructor(props: any) {
         super(props);
 
         this.state = {
             query: "",
-            lastResponse: null,
-            loading: false
+            answer: "",
+            loading: false,
+            options: []
         }
     }
 
@@ -48,39 +40,65 @@ export default class QueryHP extends React.Component<any, IQueryHPState> {
         this.setState({
             loading: true
         });
-        
-        axios.get<APIResponse>(base_uri + "/search", {
+
+        axios.get<string>(base_uri + "/answer", {
             params: {
-              q: query
+                query: query
             },
         })
-        .then((response) => {
-            console.log(response);
-            const data = response.data
-            this.setState({
-                lastResponse: data.responses[0],
-                loading: false
+            .then((response) => {
+                console.log(response);
+                this.setState({
+                    answer: response.data,
+                    loading: false
+                })
             })
-        })
-        .catch( (error) => {
-            console.log(error);
-            this.clearState();
-        });
+            .catch((error) => {
+                console.log(error);
+                this.clearState();
+            });
     }
 
     private clearState = () => {
         this.setState({
-            lastResponse: null,
+            answer: "",
             loading: false
         });
     }
 
-    private handleSubmit= () => {
+    private handleSubmit = () => {
         this.get_responses(this.state.query)
     }
 
-    private onChangeQuery = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        this.setState({query: event.target.value});
+    private onChangeQuery = (newValue: string, lookupSuggestions: boolean) => {
+        this.setState({ query: newValue });
+        console.log("Updating query to " + newValue)
+        if (lookupSuggestions) {
+            if (this.timeout) clearTimeout(this.timeout);
+            this.timeout = setTimeout(() => {
+                const query = this.state.query;
+                console.log("Searching for: " + query);
+                // this.setState({ options: [this.state.query + "the rest of suggestion"] })
+                axios.get<string>(base_uri + "/autocomplete", {
+                    params: {
+                        query: query
+                    },
+                })
+                    .then((response) => {
+                        console.log(response);
+                        const data = response.data;
+                        const suggestion = query + data;
+                        console.log("Setting options to " + suggestion);
+                        this.setState({
+                            options: [suggestion],
+                        });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        this.clearState();
+                    });
+            }, 800);
+        }
     };
 
 
@@ -90,47 +108,71 @@ export default class QueryHP extends React.Component<any, IQueryHPState> {
                 container
                 direction="column"
                 alignItems="center"
-                >
+                justifyContent="center"
+                spacing={8}
+            >
+                <Grid item>
                     <Typography variant="h3">
                         {title}
                     </Typography>
-                    <TextField
-                        className="GetToken-text"
-                        id="filled-multiline-flexible"
-                        label="Your Question"
-                        multiline
-                        fullWidth
-                        value={ this.state.query }
-                        onChange={ this.onChangeQuery }
-                        rows={2}
-                        variant="filled"
-                    />
-                    <Grid item className="create__createbuttonwrapper">
-                        <Button
-                            className={ "create__button" }
-                            disabled={ this.state.loading}
-                            variant="contained"
-                            color="primary"
-                            onClick={ this.handleSubmit }>
-                            Search
-                        </Button>
-                        { this.state.loading &&
-                            <CircularProgress size={36} className="create__createbuttonspinner" />
-                        }
+                </Grid>
+                <Grid item container>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={3}>
+                        <Grid item xs={8} md={8} >
+                            <Autocomplete
+                                id="combo-box-demo"
+                                freeSolo
+                                fullWidth
+                                disableClearable
+                                options={this.state.options}
+                                value={this.state.query}
+                                onInputChange={(event: any, newValue: string) => {
+                                    console.log("value from input chnage")
+                                    this.onChangeQuery(newValue, false);
+                                }}
+                                renderInput={(params) =>
+                                    <TextField
+                                        {...params}
+                                        className="GetToken-text"
+                                        id="filled-multiline-flexible"
+                                        label="Your Question"
+                                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                                            this.onChangeQuery(event.target.value, true);
+                                        }}
+                                        fullWidth
+                                        variant="filled"
+                                    />
+                                } />
+                        </Grid>
+                        <Grid item className="create__createbuttonwrapper" xs={2} md={2}>
+                            <Button
+                                className={"create__button"}
+                                disabled={this.state.loading}
+                                variant="contained"
+                                color="primary"
+                                onClick={this.handleSubmit}>
+                                Search
+                            </Button>
+                            {this.state.loading &&
+                                <CircularProgress size={36} className="create__createbuttonspinner" />
+                            }
+                        </Grid>
                     </Grid>
-                    {
-                        !!this.state.lastResponse &&
-                            <Grid item>
-                                <Typography variant="h3">
-                                    {this.state.lastResponse.answer}
-                                </Typography>
-                                <Typography variant="body1">
-                                    {this.state.lastResponse.passage.substring(0, this.state.lastResponse.start) }
-                                    <span className='bold'>{ this.state.lastResponse.passage.substring(this.state.lastResponse.start, this.state.lastResponse.end) }</span>
-                                    { this.state.lastResponse.passage.substring(this.state.lastResponse.end) }
-                                </Typography>
-                            </Grid>
-                    }
+                </Grid>
+                {
+                    !!this.state.answer &&
+                    <Grid item>
+                        <Typography variant="h6">Answer</Typography>
+                        <Typography variant="h4">
+                            {this.state.answer}
+                        </Typography>
+                    </Grid>
+                }
             </Grid>
         );
     }
