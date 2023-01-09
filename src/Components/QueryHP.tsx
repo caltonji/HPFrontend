98b33989c478557fb06a58d1fc724aaded706934
caltonji <caltonji@gmail.com>
2023-01-08 16:03:24 -0800

    Small changes for local commit

--- src/Components/QueryHP.tsx
+++ src/Components/QueryHP.tsx
@@ -4,7 +4,7 @@ import {
     Grid,
     CircularProgress,
     Typography
-    }
+}
     from '@material-ui/core';
 import * as React from 'react';
 import axios from 'axios';
@@ -28,7 +28,7 @@ interface IQueryHPState {
     loading: boolean
 }
 const title = "Ask a Trivia Question about Harry Potter and the Sorcerer's Stone.";
-const base_uri = "http://harrypotterb2.azurewebsites.net"
+const base_uri = "http://127.0.0.1:5000/"
 
 export default class QueryHP extends React.Component<any, IQueryHPState> {
 
@@ -48,24 +48,24 @@ export default class QueryHP extends React.Component<any, IQueryHPState> {
         this.setState({
             loading: true
         });
-        
+
         axios.get<APIResponse>(base_uri + "/search", {
             params: {
-              q: query
+                q: query
             },
         })
-        .then((response) => {
-            console.log(response);
-            const data = response.data
-            this.setState({
-                lastResponse: data.responses[0],
-                loading: false
+            .then((response) => {
+                console.log(response);
+                const data = response.data
+                this.setState({
+                    lastResponse: data.responses[0],
+                    loading: false
+                })
             })
-        })
-        .catch( (error) => {
-            console.log(error);
-            this.clearState();
-        });
+            .catch((error) => {
+                console.log(error);
+                this.clearState();
+            });
     }
 
     private clearState = () => {
@@ -75,12 +75,12 @@ export default class QueryHP extends React.Component<any, IQueryHPState> {
         });
     }
 
-    private handleSubmit= () => {
+    private handleSubmit = () => {
         this.get_responses(this.state.query)
     }
 
     private onChangeQuery = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        this.setState({query: event.target.value});
+        this.setState({ query: event.target.value });
     };
 
 
@@ -90,47 +90,47 @@ export default class QueryHP extends React.Component<any, IQueryHPState> {
                 container
                 direction="column"
                 alignItems="center"
-                >
-                    <Typography variant="h3">
-                        {title}
-                    </Typography>
-                    <TextField
-                        className="GetToken-text"
-                        id="filled-multiline-flexible"
-                        label="Your Question"
-                        multiline
-                        fullWidth
-                        value={ this.state.query }
-                        onChange={ this.onChangeQuery }
-                        rows={2}
-                        variant="filled"
-                    />
-                    <Grid item className="create__createbuttonwrapper">
-                        <Button
-                            className={ "create__button" }
-                            disabled={ this.state.loading}
-                            variant="contained"
-                            color="primary"
-                            onClick={ this.handleSubmit }>
-                            Search
-                        </Button>
-                        { this.state.loading &&
-                            <CircularProgress size={36} className="create__createbuttonspinner" />
-                        }
-                    </Grid>
-                    {
-                        !!this.state.lastResponse &&
-                            <Grid item>
-                                <Typography variant="h3">
-                                    {this.state.lastResponse.answer}
-                                </Typography>
-                                <Typography variant="body1">
-                                    {this.state.lastResponse.passage.substring(0, this.state.lastResponse.start) }
-                                    <span className='bold'>{ this.state.lastResponse.passage.substring(this.state.lastResponse.start, this.state.lastResponse.end) }</span>
-                                    { this.state.lastResponse.passage.substring(this.state.lastResponse.end) }
-                                </Typography>
-                            </Grid>
+            >
+                <Typography variant="h3">
+                    {title}
+                </Typography>
+                <TextField
+                    className="GetToken-text"
+                    id="filled-multiline-flexible"
+                    label="Your Question"
+                    multiline
+                    fullWidth
+                    value={this.state.query}
+                    onChange={this.onChangeQuery}
+                    rows={2}
+                    variant="filled"
+                />
+                <Grid item className="create__createbuttonwrapper">
+                    <Button
+                        className={"create__button"}
+                        disabled={this.state.loading}
+                        variant="contained"
+                        color="primary"
+                        onClick={this.handleSubmit}>
+                        Search
+                    </Button>
+                    {this.state.loading &&
+                        <CircularProgress size={36} className="create__createbuttonspinner" />
                     }
+                </Grid>
+                {
+                    !!this.state.lastResponse &&
+                    <Grid item>
+                        <Typography variant="h3">
+                            {this.state.lastResponse.answer}
+                        </Typography>
+                        <Typography variant="body1" className="answer_text">
+                            {this.state.lastResponse.passage.substring(0, this.state.lastResponse.start)}
+                            <span className='bold'>{this.state.lastResponse.passage.substring(this.state.lastResponse.start, this.state.lastResponse.end)}</span>
+                            {this.state.lastResponse.passage.substring(this.state.lastResponse.end)}
+                        </Typography>
+                    </Grid>
+                }
             </Grid>
         );
     }
